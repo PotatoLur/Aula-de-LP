0fcb159e9b0553644fe2abb8c1eae78d4a19a6cb
potatolur <guilherme.eguchi@sptech.school>
2025-09-17 10:59:15 -0300

    Começo aula7

---
+++ Aula7/exemplo-excecao/src/main/java/school/sptech/Divisao.java
@@ -0,0 +1,29 @@
+package school.sptech;
+
+import javax.swing.*;
+
+public class Divisao {
+    public static void main(String[] args) {
+        String sNum1, sNum2;
+
+        sNum1 = JOptionPane.showInputDialog("Digite o dividendo: ");
+        sNum2 = JOptionPane.showInputDialog("Digite o divisor: ");
+
+        try {
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
+
+            JOptionPane.showMessageDialog(null, num1 / num2);
+        } catch (ArithmeticException erro){
+            JOptionPane.showMessageDialog(null,"Divisão por zero!\n" + erro,
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (NumberFormatException erro){
+            JOptionPane.showMessageDialog(null,"Digite apenas números!\n" + erro,
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+        System.exit(0);
+    }
+}
---
+++ Aula7/exemplo-excecao/src/main/java/school/sptech/DivisaoFinally.java
@@ -0,0 +1,31 @@
+package school.sptech;
+
+import javax.swing.*;
+
+public class DivisaoFinally {
+    public static void main(String[] args) {
+        String sNum1, sNum2;
+
+        sNum1 = JOptionPane.showInputDialog("Digite o dividendo: ");
+        sNum2 = JOptionPane.showInputDialog("Digite o divisor: ");
+
+        try {
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
+
+            JOptionPane.showMessageDialog(null, num1 / num2);
+        } catch (ArithmeticException erro){
+            JOptionPane.showMessageDialog(null,"Divisão por zero!\n" + erro,
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (NumberFormatException erro){
+            JOptionPane.showMessageDialog(null,"Digite apenas números!\n" + erro,
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            JOptionPane.showMessageDialog(null,"Fim do programa");
+        }
+        
+        System.exit(0);
+    }
+}
